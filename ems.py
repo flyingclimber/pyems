c32473773ce114f1f432b26525e7f6aa062972c9
Tomasz Finc <tomasz@gmail.com>
2014-11-11 00:50:56 -0800

    Passing through the linter

--- ems.py
+++ ems.py
@@ -27,25 +27,25 @@ import usb.util
 VENDOR = 0x4670
 PRODUCT = 0x9394
 
-dev = usb.core.find(idVendor=VENDOR, idProduct=PRODUCT)
+DEV = usb.core.find(idVendor=VENDOR, idProduct=PRODUCT)
 
-if dev is None:
+if DEV is None:
     raise ValueError('Device not found')
 
-dev.set_configuration()
+DEV.set_configuration()
 
 # get an endpoint instance
-cfg = dev.get_active_configuration()
-intf = cfg[(0,0)]
+CFG = DEV.get_active_configuration()
+INTF = CFG[(0, 0)]
 
-ep = usb.util.find_descriptor(
-    intf,
+EP = usb.util.find_descriptor(
+    INTF,
     # match the first OUT endpoint
     custom_match = \
     lambda e: \
         usb.util.endpoint_direction(e.bEndpointAddress) == \
         usb.util.ENDPOINT_OUT)
 
-assert ep is not None
+assert EP is not None
 
-print dev
+print DEV
